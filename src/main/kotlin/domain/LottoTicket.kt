f7406506c266a79818283d3b0099de4def256b02
Heejun-0219 <heejun0219@gmail.com>
2024-02-04 02:37:14 +0900

    fix: LottoTicket open & throw error

--- src/main/kotlin/domain/LottoTicket.kt
+++ src/main/kotlin/domain/LottoTicket.kt
@@ -1,17 +1,17 @@
 package org.example.domain
 
-class LottoTicket(val numbers: List<Int>) {
-    fun validate(): Boolean {
+open class LottoTicket(val numbers: List<Int>) {
+    open fun validate(): Boolean {
         if (numbers.size != 6) {
-            return false
+            throw IllegalArgumentException("로또 번호는 6개여야 합니다")
         }
 
         if (numbers.toSet().size != 6) {
-            return false
+            throw IllegalArgumentException("로또 번호는 중복될 수 없습니다")
         }
 
         if (numbers.any { it !in 1..45 }) {
-            return false
+            throw IllegalArgumentException("로또 번호는 1부터 45 사이의 숫자여야 합니다")
         }
 
         return true
