bbdbee35f39dcda2e88f6f93722cb1340f9c32a7
Heejun-0219 <heejun0219@gmail.com>
2024-02-04 03:05:38 +0900

    fix: BonusLotto throw -> require

--- src/main/kotlin/domain/BonusLotto.kt
+++ src/main/kotlin/domain/BonusLotto.kt
@@ -7,13 +7,8 @@ class BonusLotto(
     override fun validate(): Boolean {
         super.validate()
 
-        if (numbers.contains(bonusNumber)) {
-            throw IllegalArgumentException("보너스 번호는 당첨 번호와 중복될 수 없습니다")
-        }
-
-        if (bonusNumber !in 1..45) {
-            throw IllegalArgumentException("보너스 번호는 1부터 45 사이의 숫자여야 합니다")
-        }
+        require(bonusNumber !in numbers) { "보너스 번호는 로또 번호와 중복되지 않아야 합니다" }
+        require(bonusNumber in 1..45) { "보너스 번호는 1부터 45 사이의 숫자여야 합니다" }
 
         return true
     }
