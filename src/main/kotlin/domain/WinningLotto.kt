a9c3ad88ca287442f649e39d41da165e8a6a8150
Heejun-0219 <heejun0219@gmail.com>
2024-02-04 23:04:31 +0900

    fix: WinningLotto

--- src/main/kotlin/domain/WinningLotto.kt
+++ src/main/kotlin/domain/WinningLotto.kt
@@ -1,11 +1,11 @@
 package org.example.domain
 
 class WinningLotto(
-    numbers: LottoTicket,
+    numbers: List<Int>,
     val bonusNumber: Int,
-) {
+) : LottoTicket(numbers) {
     init {
-        require(numbers.numbers.contains(bonusNumber).not()) { "보너스 번호는 로또 번호와 중복되지 않아야 합니다" }
+        require(bonusNumber !in numbers) { "보너스 번호는 당첨 번호와 중복되면 안됩니다" }
         require(bonusNumber in 1..45) { "보너스 번호는 1부터 45 사이의 숫자여야 합니다" }
     }
 }
