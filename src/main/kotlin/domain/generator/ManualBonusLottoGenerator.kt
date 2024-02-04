f519c17ee1661690464c4a9da2cd1c2016f8779b
Heejun-0219 <heejun0219@gmail.com>
2024-02-04 21:51:18 +0900

    fix: ManualBonusLottoGenerator 삭제

--- src/main/kotlin/domain/generator/ManualBonusLottoGenerator.kt
+++
@@ -1,9 +0,0 @@
-package org.example.domain.generator
-
-import org.example.domain.BonusLotto
-
-class ManualBonusLottoGenerator(numbers: List<Int>, private val bonusNumber: Int) : ManualLottoGenerator(numbers) {
-    override fun generate(): BonusLotto {
-        return BonusLotto(numbers, bonusNumber)
-    }
-}
