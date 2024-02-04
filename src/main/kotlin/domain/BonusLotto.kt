7d41df1c4282faff276dca4f9c1874d598ef8b5d
Heejun-0219 <heejun0219@gmail.com>
2024-02-04 12:01:20 +0900

    fix: Lotto validate -> init {}

--- src/main/kotlin/domain/BonusLotto.kt
+++ src/main/kotlin/domain/BonusLotto.kt
@@ -4,12 +4,8 @@ class BonusLotto(
     numbers: List<Int>,
     val bonusNumber: Int,
 ) : LottoTicket(numbers) {
-    override fun validate(): Boolean {
-        super.validate()
-
+    init {
         require(bonusNumber !in numbers) { "보너스 번호는 로또 번호와 중복되지 않아야 합니다" }
         require(bonusNumber in 1..45) { "보너스 번호는 1부터 45 사이의 숫자여야 합니다" }
-
-        return true
     }
 }
--- src/main/kotlin/domain/LottoTicket.kt
+++ src/main/kotlin/domain/LottoTicket.kt
@@ -1,12 +1,10 @@
 package org.example.domain
 
 open class LottoTicket(val numbers: List<Int>) {
-    open fun validate(): Boolean {
+    init {
         require(numbers.size == 6) { "로또 번호는 6개여야 합니다" }
         require(numbers.toSet().size == 6) { "로또 번호는 중복되지 않아야 합니다" }
         require(numbers.all { it in 1..45 }) { "로또 번호는 1부터 45 사이여야 합니다" }
-
-        return true
     }
 
     fun match(target: List<Int>): Int {
