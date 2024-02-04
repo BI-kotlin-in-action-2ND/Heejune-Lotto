e84c626c54d8cce557ebcb8250dfb029d5c8918c
Heejun-0219 <heejun0219@gmail.com>
2024-02-04 12:02:17 +0900

    fix: Lotto validate 함수 제거

--- src/main/kotlin/domain/generator/ManualLottoGenerator.kt
+++ src/main/kotlin/domain/generator/ManualLottoGenerator.kt
@@ -2,16 +2,8 @@ package org.example.domain.generator
 
 import org.example.domain.LottoTicket
 
-class ManualLottoGenerator(
-    private val numbers: List<Int>,
+open class ManualLottoGenerator(
+    val numbers: List<Int>,
 ) : LottoGenerator {
-    override fun generate(): LottoTicket {
-        val lottoTicket = LottoTicket(numbers)
-
-        if (!lottoTicket.validate()) {
-            throw IllegalArgumentException("로또 티켓의 유효성 검사 실패")
-        }
-
-        return lottoTicket
-    }
+    override fun generate(): LottoTicket = LottoTicket(numbers)
 }
