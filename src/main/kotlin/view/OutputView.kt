525390c127e0a923d1135244935171e928294351
Heejun-0219 <heejun0219@gmail.com>
2024-02-04 23:56:27 +0900

    refactor: OutputView 출력 기능만 하도록 변경

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -11,8 +11,11 @@ object OutputView {
     private const val SUM_MANUAL_AUTO_TICKET = "수동으로 %d장, 자동으로 %d장을 구매했습니다."
     private const val TOTAL_USER_LOTTO = "-- 사용자 로또 번호 --"
     private const val RESULT_LOTTO = "당첨 통계\n---"
-    private const val RANK_BEHIND = "등 ("
-    private const val RESULT_PRIZE_KW = " KW) -"
+    private const val FIRST_MESSAGE = "1등 [6개 번호 일치"
+    private const val SECOND_MESSAGE = "2등 [5개 번호 일치, 보너스 번호 일치"
+    private const val THIRD_MESSAGE = "3등 [5개 번호 일치"
+    private const val FOURTH_MESSAGE = "4등 [4개 번호 일치"
+    private const val NONE_MESSAGE = ""
     private const val RESULT_COUNT = "개"
     private const val RESULT_EARNINGS = "총 수익률은"
     private const val RESULT_EARNINGS_PERCENT = "%입니다."
@@ -35,27 +38,24 @@ object OutputView {
         userLottoTickets.forEach { println(it.numbers.joinToString(NUMBERS_DELIMITER, PREFIX_MESSAGE, SUFFIX_MESSAGE)) }
     }
 
-    fun displayPrizeResults(results: Map<PrizeCategory, Int>): Int {
-        var totalPrize = 0
+    fun displayPrizeResults(results: Map<PrizeCategory, Int>) {
         println(RESULT_LOTTO)
         PrizeCategory.entries.forEach { category ->
             if (category != PrizeCategory.NONE) {
                 val count = results[category] ?: 0
                 val description = getCategoryDescription(category)
                 println("$description ${formatPrize(category.prize)}] - $count${RESULT_COUNT}")
-                totalPrize += category.prize * count
             }
         }
-        return totalPrize
     }
 
     private fun getCategoryDescription(category: PrizeCategory): String {
         return when (category) {
-            PrizeCategory.FIRST -> "1등 [6개 번호 일치"
-            PrizeCategory.SECOND -> "2등 [5개 번호 일치, 보너스 번호 일치"
-            PrizeCategory.THIRD -> "3등 [5개 번호 일치"
-            PrizeCategory.FOURTH -> "4등 [4개 번호 일치"
-            PrizeCategory.NONE -> ""
+            PrizeCategory.FIRST -> FIRST_MESSAGE
+            PrizeCategory.SECOND -> SECOND_MESSAGE
+            PrizeCategory.THIRD -> THIRD_MESSAGE
+            PrizeCategory.FOURTH -> FOURTH_MESSAGE
+            PrizeCategory.NONE -> NONE_MESSAGE
         }
     }
 
@@ -63,12 +63,8 @@ object OutputView {
         return "(%,dKW)".format(prize)
     }
 
-    fun displayTotalEarning(
-        totalPrize: Int,
-        purchaseAmount: Int,
-    ) {
-        val profitRate = (totalPrize.toDouble() / purchaseAmount * 100).toInt()
-        println("\n$RESULT_EARNINGS $profitRate$RESULT_EARNINGS_PERCENT")
+    fun displayTotalEarning(totalEarning: Double) {
+        println("$RESULT_EARNINGS ${totalEarning}${RESULT_EARNINGS_PERCENT}")
     }
 
     fun displayInvalidInput(message: String?) {
