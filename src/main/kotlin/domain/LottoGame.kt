2ab4d4d7a65abc496a305e0ae9b9050d70f3562e
Heejun-0219 <heejun0219@gmail.com>
2024-02-05 10:24:50 +0900

    refactor: checkRank 함수로 객체가 일하도록 변경

--- src/main/kotlin/domain/LottoGame.kt
+++ src/main/kotlin/domain/LottoGame.kt
@@ -4,13 +4,25 @@ class LottoGame(
     private val userLottoTickets: List<LottoTicket>,
     private val winningLotto: WinningLotto,
 ) {
-    fun calculateResults(): List<Pair<Int, Boolean>> {
+    fun calculateResults(): Map<PrizeCategory, Int> {
         val result: MutableList<Pair<Int, Boolean>> = mutableListOf()
         userLottoTickets.forEach { lottoTicket ->
             val matchCount = lottoTicket.match(winningLotto.numbers)
             val hasBonus = lottoTicket.hasBonus(winningLotto.bonusNumber)
-            result.add(matchCount to hasBonus)
+            result.add(Pair(matchCount, hasBonus))
         }
-        return result
+        return checkRank(result)
+    }
+
+    private fun checkRank(results: List<Pair<Int, Boolean>>): Map<PrizeCategory, Int> {
+        return results.groupBy { (matchCount, hasBonus) ->
+            when {
+                matchCount == 6 -> PrizeCategory.FIRST
+                matchCount == 5 && hasBonus -> PrizeCategory.SECOND
+                matchCount == 5 -> PrizeCategory.THIRD
+                matchCount == 4 -> PrizeCategory.FOURTH
+                else -> PrizeCategory.NONE
+            }
+        }.mapValues { (_, value) -> value.size }.filterKeys { it != PrizeCategory.NONE }
     }
 }
