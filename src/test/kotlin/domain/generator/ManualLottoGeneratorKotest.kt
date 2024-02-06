2760c4fe127b825a176a08efa8aa6c68042c2e00
Heejun-0219 <heejun0219@gmail.com>
2024-02-06 14:30:00 +0900

    test: ManualLottoGeneratorKotest 테스트 추가

---
+++ src/test/kotlin/domain/generator/ManualLottoGeneratorKotest.kt
@@ -0,0 +1,37 @@
+package domain.generator
+
+import io.kotest.assertions.throwables.shouldThrowExactly
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import org.example.domain.generator.ManualLottoGenerator
+
+class ManualLottoGeneratorKotest : StringSpec({
+    val generator = ManualLottoGenerator()
+
+    "수동 로또 번호 생성 테스트" {
+        val validNumbers = listOf(1, 2, 3, 4, 5, 6)
+        val lottoTicket = generator.generate(validNumbers)
+        lottoTicket.numbers shouldBe validNumbers
+    }
+
+    "수동 로또 번호 생성 중복 실패 테스트" {
+        val invalidNumbers = listOf(1, 2, 3, 4, 5, 5)
+        shouldThrowExactly<IllegalArgumentException> {
+            generator.generate(invalidNumbers)
+        }
+    }
+
+    "수동 로또 번호 생성 범위 실패 테스트" {
+        val outOfRangeNumbers = listOf(0, 2, 3, 4, 5, 46)
+        shouldThrowExactly<IllegalArgumentException> {
+            generator.generate(outOfRangeNumbers)
+        }
+    }
+
+    "수동 로또 번호 생성 갯수 실패 테스트" {
+        val lessThanSixNumbers = listOf(1, 2, 3, 4, 5)
+        shouldThrowExactly<IllegalArgumentException> {
+            generator.generate(lessThanSixNumbers)
+        }
+    }
+})
