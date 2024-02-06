49a21788918d22928b9adc38b592be20891eca8c
Heejun-0219 <heejun0219@gmail.com>
2024-02-06 16:35:02 +0900

    feat: BonusLottoNumbersCheck value class 생성

---
+++ src/main/kotlin/domain/BonusLottoNumbersCheck.kt
@@ -0,0 +1,11 @@
+package org.example.domain
+
+import org.example.config.LottoConfig
+
+@JvmInline
+value class BonusLottoNumbersCheck(private val numbers: List<Int>) {
+    init {
+        require(numbers.distinct().size == 7) { LottoConfig.ERROR_MESSAGE_DUPLICATE }
+        require(numbers.all { it in (LottoConfig.START_NUMBER..LottoConfig.END_NUMBER) }) { LottoConfig.ERROR_MESSAGE_NUMBER_RANGE }
+    }
+}
--- src/main/kotlin/domain/WinningLotto.kt
+++ src/main/kotlin/domain/WinningLotto.kt
@@ -5,7 +5,6 @@ class WinningLotto(
     val bonusNumber: Int,
 ) : LottoTicket(numbers) {
     init {
-        require(bonusNumber !in numbers) { "보너스 번호는 당첨 번호와 중복되면 안됩니다" }
-        require(bonusNumber in 1..45) { "보너스 번호는 1부터 45 사이의 숫자여야 합니다" }
+        BonusLottoNumbersCheck(numbers + bonusNumber)
     }
 }
