3f4d5560151cfb00b5daf0e470c03f3d0c19cdfe
Heejun-0219 <heejun0219@gmail.com>
2024-02-06 13:39:28 +0900

    refactor: 테스트 단위 쪼개기

--- src/test/kotlin/domain/WinningLottoTest.kt
+++ src/test/kotlin/domain/WinningLottoTest.kt
@@ -21,22 +21,58 @@ class WinningLottoTest {
                 WinningLotto(listOf(1, 2, 3, 4, 5), 7)
             }.message!!.contains("6개여야 합니다"),
         )
+    }
 
+    @Test
+    fun `lotto failure for duplicate`() {
         assertTrue(
             assertThrows<IllegalArgumentException> {
                 WinningLotto(listOf(1, 2, 3, 4, 5, 5), 7)
             }.message!!.contains("중복"),
         )
+    }
 
+    @Test
+    fun `lotto failure for bonus`() {
+        assertTrue(
+            assertThrows<IllegalArgumentException> {
+                WinningLotto(listOf(1, 2, 3, 4, 5, 6), 6)
+            }.message!!.contains("중복"),
+        )
+    }
+
+    @Test
+    fun `lotto failure for number over range`() {
         assertTrue(
             assertThrows<IllegalArgumentException> {
                 WinningLotto(listOf(1, 2, 3, 4, 5, 46), 7)
             }.message!!.contains("1부터 45"),
         )
+    }
 
+    @Test
+    fun `lotto failure for number under range`() {
         assertTrue(
             assertThrows<IllegalArgumentException> {
                 WinningLotto(listOf(0, 2, 3, 4, 5, 6), 7)
+            }.message!!.contains("1부터 45"),
+        )
+    }
+
+    @Test
+    fun `lotto failure for bonus under range`() {
+        assertTrue(
+            assertThrows<IllegalArgumentException> {
+                WinningLotto(listOf(1, 2, 3, 4, 5, 6), 0)
+            }.message!!.contains("1부터 45"),
+        )
+    }
+
+    @Test
+    fun `lotto failure for bonus over range`() {
+        assertTrue(
+            assertThrows<IllegalArgumentException> {
+                WinningLotto(listOf(1, 2, 3, 4, 5, 6), 46)
             }.message!!.contains("1부터 45"),
         )
     }
