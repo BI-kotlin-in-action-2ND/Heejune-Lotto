8822282023809d2a1b98f5363b432209912b8c50
Heejun-0219 <heejun0219@gmail.com>
2024-02-06 15:14:56 +0900

    fix: 생성시 정려되도록 변경

--- src/main/kotlin/domain/generator/ManualLottoGenerator.kt
+++ src/main/kotlin/domain/generator/ManualLottoGenerator.kt
@@ -3,7 +3,9 @@ package org.example.domain.generator
 import org.example.domain.LottoTicket
 
 open class ManualLottoGenerator : LottoGenerator {
-    fun generate(numbers: List<Int>): LottoTicket = LottoTicket(numbers.sorted())
+    fun generate(manualNumbers: List<List<Int>>): List<LottoTicket> {
+        return manualNumbers.map { LottoTicket(it.sorted()) }
+    }
 
     override fun generate(): LottoTicket {
         throw UnsupportedOperationException("수동 로또 생성기는 수동으로 번호를 입력해야 합니다")
