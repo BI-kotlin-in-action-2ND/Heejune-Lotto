ee40eeb47cee3d4ed8351e3969014081bc1a5ea5
Heejun-0219 <heejun0219@gmail.com>
2024-02-08 16:35:42 +0900

    fix: PrizeCategory 매개변수 및 동반 객체 수정

--- src/main/kotlin/domain/PrizeCategory.kt
+++ src/main/kotlin/domain/PrizeCategory.kt
@@ -1,23 +1,17 @@
 package org.example.domain
 
-enum class PrizeCategory(val prize: Int) {
-    FIRST(100_000),
-    SECOND(50_000),
-    THIRD(100),
-    FOURTH(5),
-    NONE(0),
+enum class PrizeCategory(val prize: Int, val matchCount: Int, val hasBonus: Boolean) {
+    FIRST(100_000, 6, false),
+    SECOND(50_000, 5, true),
+    THIRD(100, 5, false),
+    FOURTH(5, 4, false),
+    NONE(0, 0, false),
     ;
 
     companion object {
         fun getRank(
             matchCount: Int,
             hasBonus: Boolean,
-        ) = when {
-            matchCount == 6 -> FIRST
-            matchCount == 5 && hasBonus -> SECOND
-            matchCount == 5 -> THIRD
-            matchCount == 4 -> FOURTH
-            else -> NONE
-        }
+        ) = entries.find { it.matchCount == matchCount && it.hasBonus == hasBonus } ?: NONE
     }
 }
