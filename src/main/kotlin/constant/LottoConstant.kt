85660b03ee111c3864c178927a74c2c8296d738c
Heejun-0219 <heejun0219@gmail.com>
2024-02-08 15:34:13 +0900

    refactor: Error Message 수정

--- src/main/kotlin/constant/LottoConstant.kt
+++ src/main/kotlin/constant/LottoConstant.kt
@@ -4,10 +4,8 @@ object LottoConstant {
     const val START_NUMBER = 1
     const val END_NUMBER = 45
     const val LOTTO_TICKET_SIZE = 6
-    const val ERROR_MESSAGE_LOTTO_SIZE = "로또 번호는 6개여야 합니다."
-    const val ERROR_MESSAGE_DUPLICATE = "로또 번호는 중복되지 않아야 합니다."
+    const val ERROR_MESSAGE_LOTTO_INPUT = "로또 번호는 6개이고, 중복되지 않는 숫자여야 합니다."
     const val ERROR_MESSAGE_NUMBER_RANGE = "로또 번호는 1부터 45 사이여야 합니다."
-    const val MANUAL_GENERATOR_NOT_SUPPORTED = "수동 로또 생성기는 수동으로 번호를 입력해야 합니다"
     const val BLANK = ""
     const val AUTO_MODE = "A"
 }
