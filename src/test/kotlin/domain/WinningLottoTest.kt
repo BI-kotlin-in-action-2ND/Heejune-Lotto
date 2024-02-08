e19d9bd8157826e0a85256761668cd0cb0eb9ec9
Heejun-0219 <heejun0219@gmail.com>
2024-02-08 15:24:02 +0900

    test: kotest 수정

--- src/test/kotlin/domain/WinningLottoTest.kt
+++ src/test/kotlin/domain/WinningLottoTest.kt
@@ -11,38 +11,38 @@ class WinningLottoTest : FunSpec({
     context("WinningLotto 유효성 검증") {
         test("로또 및 보너스 생성 테스트") {
             assertDoesNotThrow {
-                WinningLotto(listOf(1, 2, 3, 4, 5, 6), 7)
+                WinningLotto(setOf(1, 2, 3, 4, 5, 6), 7)
             }
         }
 
         test("로또 번호 실패 테스트 - 숫자 개수 미달") {
             val exception =
                 shouldThrowExactly<IllegalArgumentException> {
-                    WinningLotto(listOf(1, 2, 3, 4, 5), 7)
+                    WinningLotto(setOf(1, 2, 3, 4, 5), 7)
                 }
-            exception.message shouldContain LottoConstant.ERROR_MESSAGE_LOTTO_SIZE
+            exception.message shouldContain LottoConstant.ERROR_MESSAGE_LOTTO_INPUT
         }
 
         test("로또 번호 실패 테스트 - 중복 숫자") {
             val exception =
                 shouldThrowExactly<IllegalArgumentException> {
-                    WinningLotto(listOf(1, 2, 3, 4, 5, 5), 7)
+                    WinningLotto(setOf(1, 2, 3, 4, 5, 5), 7)
                 }
-            exception.message shouldContain LottoConstant.ERROR_MESSAGE_DUPLICATE
+            exception.message shouldContain LottoConstant.ERROR_MESSAGE_LOTTO_INPUT
         }
 
         test("보너스 번호 실패 테스트 - 보너스 번호 중복") {
             val exception =
                 shouldThrowExactly<IllegalArgumentException> {
-                    WinningLotto(listOf(1, 2, 3, 4, 5, 6), 6)
+                    WinningLotto(setOf(1, 2, 3, 4, 5, 6), 6)
                 }
-            exception.message shouldContain LottoConstant.ERROR_MESSAGE_DUPLICATE
+            exception.message shouldContain LottoConstant.ERROR_MESSAGE_LOTTO_INPUT
         }
 
         test("로또 번호 실패 테스트 - 범위를 벗어난 숫자") {
             val exception =
                 shouldThrowExactly<IllegalArgumentException> {
-                    WinningLotto(listOf(1, 2, 3, 4, 5, 46), 7)
+                    WinningLotto(setOf(1, 2, 3, 4, 5, 46), 7)
                 }
             exception.message shouldContain LottoConstant.ERROR_MESSAGE_NUMBER_RANGE
         }
@@ -50,7 +50,7 @@ class WinningLottoTest : FunSpec({
         test("로또 번호 실패 테스트 - 범위 아래 숫자") {
             val exception =
                 shouldThrowExactly<IllegalArgumentException> {
-                    WinningLotto(listOf(0, 2, 3, 4, 5, 6), 7)
+                    WinningLotto(setOf(0, 2, 3, 4, 5, 6), 7)
                 }
             exception.message shouldContain LottoConstant.ERROR_MESSAGE_NUMBER_RANGE
         }
@@ -58,7 +58,7 @@ class WinningLottoTest : FunSpec({
         test("보너스 번호 실패 테스트 - 범위 아래 보너스 번호") {
             val exception =
                 shouldThrowExactly<IllegalArgumentException> {
-                    WinningLotto(listOf(1, 2, 3, 4, 5, 6), 0)
+                    WinningLotto(setOf(1, 2, 3, 4, 5, 6), 0)
                 }
             exception.message shouldContain LottoConstant.ERROR_MESSAGE_NUMBER_RANGE
         }
@@ -66,7 +66,7 @@ class WinningLottoTest : FunSpec({
         test("보너스 번호 실패 테스트 - 범위를 벗어난 보너스 번호") {
             val exception =
                 shouldThrowExactly<IllegalArgumentException> {
-                    WinningLotto(listOf(1, 2, 3, 4, 5, 6), 46)
+                    WinningLotto(setOf(1, 2, 3, 4, 5, 6), 46)
                 }
             exception.message shouldContain LottoConstant.ERROR_MESSAGE_NUMBER_RANGE
         }
