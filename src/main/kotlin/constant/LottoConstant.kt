8719495bd8f85fa1046656936550ec4bb14ab831
Heejun-0219 <heejun0219@gmail.com>
2024-02-24 18:11:45 +0900

    feat: BONUS_TICKET_SIZE 출력 생성

--- src/main/kotlin/constant/LottoConstant.kt
+++ src/main/kotlin/constant/LottoConstant.kt
@@ -4,6 +4,7 @@ object LottoConstant {
     const val START_NUMBER = 1
     const val END_NUMBER = 45
     const val LOTTO_TICKET_SIZE = 6
+    const val BONUS_TICKET_SIZE = 7
     const val ERROR_MESSAGE_LOTTO_INPUT = "로또 번호는 6개이고, 중복되지 않는 숫자여야 합니다."
     const val ERROR_MESSAGE_NUMBER_RANGE = "로또 번호는 1부터 45 사이여야 합니다."
     const val BLANK = ""
