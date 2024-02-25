18217c06cd518dd9497a7169f22cc7320af9e9a5
Heejun-0219 <heejun0219@gmail.com>
2024-02-25 16:26:38 +0900

    fix: value class 삭제

--- src/main/kotlin/domain/ticket/BonusLottoNumbers.kt
+++
@@ -1,9 +0,0 @@
-package org.example.domain.ticket
-
-import org.example.constant.LottoConstant
-
-class BonusLottoNumbers(private val numbers: Set<LottoNumber>) {
-    init {
-        require(numbers.distinct().size == LottoConstant.BONUS_TICKET_SIZE) { LottoConstant.ERROR_MESSAGE_LOTTO_INPUT }
-    }
-}
